e3d2f21f87c6ee04618ac4d3b69f84450039e592
chuper <chuper@gmx.fr>
2022-12-26 17:03:51 +0100

    filtre et login terminé

--- FrontEnd/assets/JavaScript/script.js
+++ FrontEnd/assets/JavaScript/script.js
@@ -3,21 +3,22 @@ let projects = []
 let projectsFiltered = []
 
 const projetsApi = fetch('http://localhost:5678/api/works')
-.then(function(response){
+  .then(function (response) {
     if (response.ok) {
       console.log(response)
-    return response.json(); }
+      return response.json();
+    }
   })
-  .then(function(data){
+  .then(function (data) {
     console.log(data)
     afficherProjects(data)
     projects = data
     projectsFiltered = data
   })
-  .catch(function(err) {
-    // Affiche une erreur est survenue
-    return alert("une erreur est survenue.")
-  });
+/*.catch(function(err) {
+  // Affiche une erreur est survenue
+  return alert("une erreur est survenue.")
+});*/
 
 
 /////////////AFFICHAGE DES PROJETS/////////////
@@ -26,22 +27,26 @@ const figure = document.getElementsByTagName("figure")
 
 
 function afficherProjects(projects) {
+  //supprime les travaux avant d'afficher ceux selectionner, evite les doublons//
+  document.querySelector(".gallery").innerHTML = ''
+
   for (let project of projects) {
     let figure = document.createElement("figure");
     gallery.appendChild(figure)
-    
+
     let image = document.createElement("img")
     image.setAttribute("src", project.imageUrl)
     image.setAttribute("alt", project.title)
     image.setAttribute("crossorigin", "anonymous")
     figure.appendChild(image)
-    
+
     let figcap = document.createElement("figcaption");
     figcap.innerText = project.title
     figure.appendChild(figcap)
 
   }
 }
+
 
 /////////////MENU DE SELECTION DES PROJETS/////////////
 
@@ -56,24 +61,24 @@ const btn_filters = document.querySelectorAll(".btn_filter")
 
 for (const btn of btn_filters) {
   btn.addEventListener("click", () => {
-  const getValue = btn.getAttribute("value");
+    const getValue = btn.getAttribute("value");
 
- if (getValue === "Tous"){
-  return  result = projectsFiltered
-}
-else {
-   result = projectsFiltered.filter(function(projet) {  
-    
-    return projet.category.name === getValue;
+    if (getValue === "Tous") {
+      projectsFiltered = projects
+    }
+    else {
+      projectsFiltered = projects.filter(function (projet) {
+        return projet.category.name === getValue;
+      })
+
+    }
+    afficherProjects(projectsFiltered)
+
+    console.log(projectsFiltered)
   })
-}
-
-console.log(result)
-    })
 
 
 }
-
 
 
 /*const result = projectsFiltered.filter(function(projet) {   
